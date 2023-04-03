a301cf44131fd5e02358ba6bbcfad8cbff05a3ea
Fendrik Nicholas <fendriknicholas@Fendriks-MacBook-Pro.local>
2023-04-03 22:57:12 +0700

    added interceptor

--- src/main/java/com/java/javagrpc/GrpcClientInterceptor.java
+++ src/main/java/com/java/javagrpc/GrpcClientInterceptor.java
@@ -1,22 +1,42 @@
 package com.java.javagrpc;
 
-import io.grpc.*;
-import io.grpc.internal.GrpcAttributes;
-import io.grpc.internal.GrpcUtil;
+import io.grpc.Context;
+import io.grpc.Contexts;
+import io.grpc.Grpc;
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
 import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @Slf4j
 @GrpcGlobalServerInterceptor
 public class GrpcClientInterceptor implements ServerInterceptor {
+    public static final Context.Key<String> CLIENT_IP_KEY = Context.key("client-ip");
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
-        String clientIp = serverCall.getAttributes().get(Grpc.TRANSPORT_ATTR_LOCAL_ADDR).toString();
+        String clientIp = extractIpAddress(serverCall.getAttributes().get(Grpc.TRANSPORT_ATTR_LOCAL_ADDR).toString());
         log.info("Received request from client with IP address {}", clientIp);
 
-        return serverCallHandler.startCall(serverCall, metadata);
+        Context context = Context.current().withValue(CLIENT_IP_KEY, clientIp);
+        return Contexts.interceptCall(context, serverCall, metadata, serverCallHandler);
+       // return serverCallHandler.startCall(serverCall, metadata);
+    }
+
+    private String extractIpAddress(String fullAddress) {
+        Pattern pattern = Pattern.compile("/(\\d+\\.\\d+\\.\\d+\\.\\d+):\\d+");
+        Matcher matcher = pattern.matcher(fullAddress);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/com/java/javagrpc/ProductService.java
+++ src/main/java/com/java/javagrpc/ProductService.java
@@ -3,24 +3,28 @@ package com.java.javagrpc;
 import com.javarpc.spring.grpc.proto.Identity;
 import com.javarpc.spring.grpc.proto.Product;
 import com.javarpc.spring.grpc.proto.ProductRequest;
-import com.javarpc.spring.grpc.proto.ProductServiceGrpc;
+import com.javarpc.spring.grpc.proto.ProductServiceGrpc;;
+import io.grpc.Context;
+import io.grpc.Metadata;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @GrpcService
 @Slf4j
 public class ProductService extends ProductServiceGrpc.ProductServiceImplBase {
     private List<StreamObserver<Product>> activeObservers = new CopyOnWriteArrayList<>();
+    private static ConcurrentHashMap<Identity, String> clientIp = new ConcurrentHashMap<>();
     private Random random = new Random();
     @Override
     public void getProduct(ProductRequest request, StreamObserver<Product> responseObserver) {
-
         System.out.println("Request: " + request);
         Product product = Product.newBuilder()
                 .setId(request.getId())
@@ -40,7 +44,8 @@ public class ProductService extends ProductServiceGrpc.ProductServiceImplBase {
     @Override
     public void listenProductStream(Identity identity, StreamObserver<Product> responseObserver){
         try{
-            log.info("identity: {}", identity.getId());
+            String clientIp = GrpcClientInterceptor.CLIENT_IP_KEY.get();
+            log.info("identity: {}, with ip: {}", identity.getId(), clientIp);
             if(Integer.parseInt(identity.getId()) > 5){
                 activeObservers.add(responseObserver);
             }
